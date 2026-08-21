b32c2c0b98ff2bfec529b0d5e66a9029f4b5b795
PapaMike80 <70916601+PapaMike80@users.noreply.github.com>
2026-08-21 09:21:18 +0200

    Aggiorna cache pagina Navi

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 
 // Cambio obbligatorio dopo il ripristino dello script popup: invalida anche
 // le copie memorizzate dalla PWA e da Safari.
-const CACHE_VERSION = 'navisuite-v173-home-navi';
+const CACHE_VERSION = 'navisuite-v174-navi-mobile-menu';
 const CACHE_NAME = CACHE_VERSION;
 
 // File statici da pre-caricare durante l'installazione.
