b5ebd34bd7d947b3c3ae668c1327604d728365ab
PapaMike80 <70916601+PapaMike80@users.noreply.github.com>
2026-08-25 17:44:14 +0200

    Esclude DT dai rifornimenti equipaggio

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 
 // Cambio obbligatorio dopo il ripristino dello script popup: invalida anche
 // le copie memorizzate dalla PWA e da Safari.
-const CACHE_VERSION = 'navibeta-v184-bolle-servizi-colorate';
+const CACHE_VERSION = 'navibeta-v185-rifornimenti-equipaggio';
 const CACHE_NAME = CACHE_VERSION;
 
 // File statici da pre-caricare durante l'installazione.
