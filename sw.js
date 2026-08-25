8e531bf4be78856feba3499214612c7b783be9e5
PapaMike80 <70916601+PapaMike80@users.noreply.github.com>
2026-08-25 17:13:01 +0200

    Colora le bolle dei servizi

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 
 // Cambio obbligatorio dopo il ripristino dello script popup: invalida anche
 // le copie memorizzate dalla PWA e da Safari.
-const CACHE_VERSION = 'navibeta-v183-competenze-residenze';
+const CACHE_VERSION = 'navibeta-v184-bolle-servizi-colorate';
 const CACHE_NAME = CACHE_VERSION;
 
 // File statici da pre-caricare durante l'installazione.
